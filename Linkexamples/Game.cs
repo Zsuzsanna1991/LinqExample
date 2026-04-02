a5e77ec934e19746300959b52e53c0855ee91ccc
Zsuzsanna1991 <zsuzsannaszabo04@gmail.com>
2026-04-02 09:26:37 +0200

    Initial commit.

---
+++ Linkexamples/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkexamples
+{
+    internal class Game
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
+        public int Price { get; set; }
+
+    }
+}
